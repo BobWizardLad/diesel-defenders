61efd022852d3d0540e4d55d63d110a6563a3cc2
BobWizardLad <bobwizardlad@Gmail.com>
2023-06-14 19:51:35 -0500

    Pawn Movement Scripting
    Pawns can now move based on a list of grid coordinates! This will be a stepping stone towards an eventual A* search to give the coordinate list.

--- diesel-defenders/Grid.gd
+++ diesel-defenders/Grid.gd
@@ -4,21 +4,24 @@ class_name Grid
 extends Resource
 
 # Size of the grid: AxB sized game board
-@export var size = Vector2(16, 12)
+@export var size: Vector2 = Vector2(16, 12)
+# Size of offset from global root to grid
+@export var grid_offset: Vector2 = Vector2(128, 128)
 # Size of each cell in pixels: AxA cell dimension
-@export var cell_size = 32
+@export var cell_size: Vector2 = Vector2(32, 32)
 var _cell_half = cell_size / 2
+var _tile_offset = grid_offset / cell_size
 
 # Input the inner-grid position, get the map location by pixels
 # Used to snap nodes into the grid system
 func get_grid_snap(grid_position: Vector2) -> Vector2:
-	return grid_position * cell_size + _cell_half
+	return (grid_position * cell_size + _cell_half) + grid_offset
 
 # Input a pixel position on map, get the grid tile it is in
 # Used to relate map coordinates to grid tiles
 # Used with get_grid_snap to place objects
 func get_map_to_grid(map_position: Vector2) -> Vector2:
-	return (map_position / cell_size).floor()
+	return ((map_position + _tile_offset)/ cell_size).floor()
 
 # Checker to see if inner-grid coordinate is within bounds of the play area
 func is_within_bounds(cell_coordinates: Vector2) -> bool:
---
+++ diesel-defenders/Unit.gd
@@ -0,0 +1,88 @@
+# Pawn on the game board
+# The board will manage the unit's position, and the master will manage the unit's state.
+# This is a visual representation of the game state that grid & master manipulates
+class_name Unit
+extends Path2D
+
+@export var grid: Resource = load("res://Grid.tres")
+
+@export var move_range = 6
+@export var move_speed = 50.0
+@export var skin: Texture
+
+signal walk_finished
+
+# Cell is location IN GRID not pixels
+var cell = Vector2.ZERO
+var is_selected = false
+var is_walking = false
+
+@onready var _sprite: Sprite2D = $Path/Sprite
+@onready var _animation_player: AnimationPlayer = $AnimationPlayer
+@onready var _path_follow: PathFollow2D = $Path
+
+# Setter for unit sprite
+func set_skin(texture: Texture) -> void:
+	skin = texture
+
+# Setter that places units on grid
+# Ensures snap to grid and within bounds
+func set_cell(coord: Vector2) -> void:
+	cell = grid.is_within_bounds(coord)
+
+# Setter for the is unit selected flag
+func set_is_selected(value: bool) -> void:
+	is_selected = value
+	if is_selected:
+		pass
+	else:
+		pass
+
+# Setter for the is unit walking (move & animate) flag
+func set_is_walking(value: bool) -> void:
+	is_walking = value
+	set_process(is_walking)
+
+# Prepare the game
+# Snap the unit into the grid system via functions from Grid
+func _ready() -> void:
+	# Delay the execution of _process so that we only move when we want to
+	set_process(false)
+	
+	cell = grid.get_map_to_grid(position)
+	position = grid.get_grid_snap(cell)
+	
+	curve = Curve2D.new()
+	
+	# -------- TEST ZONE ---------------
+	var points := [
+		Vector2(0, 2),
+		Vector2(4, 2),
+		Vector2(4, 4),
+		Vector2(6, 4),
+	]
+	walk_along(PackedVector2Array(points))
+	# --------- ALERTA -----------------
+
+func _process(delta: float) -> void:
+	# Progress along the path curve
+	_path_follow.progress += move_speed * delta
+	
+	# If we reach 100% of the path (complete the trip), reset at new cell
+	if _path_follow.progress_ratio >= 1.0:
+		self.set_is_walking(false)
+		_path_follow.progress = 0.0
+		position = grid.get_grid_snap(cell)
+		curve.clear_points()
+		
+		emit_signal("walk_finished")
+
+func walk_along(path: PackedVector2Array) -> void:
+	if path.is_empty():
+		return
+	curve.add_point(Vector2.ZERO)
+	for point in path:
+		curve.add_point(grid.get_grid_snap(point) - position)
+	
+	cell = path[-1]
+	self.set_is_walking(true)
